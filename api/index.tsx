4f347bb6ecbc6582dd89f97ba859bb78db2360ae
Tushar Ojha <tusharojha2001@gmail.com>
2024-08-27 18:06:40 +0530

    reverse sign test2

--- api/index.tsx
+++ api/index.tsx
@@ -21,42 +21,9 @@ app.frame('/', (c) => {
   const { buttonValue, inputText, status } = c
   const fruit = inputText || buttonValue
   return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
-        }}
-      >
-        <div
-          style={{
-            color: 'white',
-            fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
-            lineHeight: 1.4,
-            marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
-          }}
-        >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
-        </div>
-      </div>
-    ),
+
+    image: 'https://i.ytimg.com/vi/R3UACX5eULI/maxresdefault.jpg',
+    
     intents: [
       <TextInput placeholder="Enter custom fruit..." />,
       <Button value="apples">Apples</Button>,
